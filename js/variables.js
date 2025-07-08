57061bfc86fcbc811a11937aecd8d0ccb96b818c
Igor Suvorov <i.suvorov@monto.com.ua>
2025-07-08 17:43:12 +0300

    first commit

---
+++ js/variables.js
@@ -0,0 +1,13 @@
+let first_name = "Igor";
+let birth_year = 1993;
+let isLearningJS = true;
+let age = 2025 - birth_year;
+let info = 'Привет, '+ first_name + '! Тебе ' + age + ' лет.';
+
+console.log(first_name) // Output: Igor
+console.log(birth_year) // Output: 1993
+console.log(isLearningJS) // Output: true
+console.log(typeof first_name) // Output: string
+console.log(typeof birth_year) // Output: number
+console.log(typeof isLearningJS) // Output: boolean
+console.log(typeof age) // Output: number
